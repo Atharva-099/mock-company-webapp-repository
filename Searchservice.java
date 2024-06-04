3c8ca1bfbe0188258a5687853d1fea685dc397db
Atharva-099 <155075615+Atharva-099@users.noreply.github.com>
2024-06-05 01:58:24 +0530

    Searchservice.java

---
+++ Searchservice.java
@@ -0,0 +1,8 @@
+import java.util.Collections;
+import java.util.List;
+
+public class SearchService {
+    public List<Product> searchProducts(String query) {
+        return Collections.emptyList();
+    }
+}
